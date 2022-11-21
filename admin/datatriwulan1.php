107c9663a699b91aca1da2a6bcfbe86037314bae
Dhiradhim <dhiradhim@gmail.com>
2022-11-21 13:55:47 +0800

    main

--- admin/datatriwulan1.php
+++ admin/datatriwulan1.php
@@ -13,7 +13,7 @@ $tahun = $_SESSION['tahun'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Data Triwulan 4 - Tahun <?=$tahun?></title>
+    <title>Data Triwulan 1 - Tahun <?=$tahun?></title>
 
 <?php
 include('head.html');
