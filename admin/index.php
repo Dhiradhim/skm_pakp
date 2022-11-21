1540af1bc8e9499acf0c2218bc6b0d28a1c80d0d
Dhiradhim <dhiradhim@gmail.com>
2022-11-21 13:34:14 +0800

    main

--- admin/index.php
+++ admin/index.php
@@ -40,19 +40,7 @@ include('head.html');
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>Data SKM dan IPAK Triwulan 4</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                          </div>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
+                    
                     <div class="clearfix"></div>
                   </div>
                   <!-- QUERY -->
@@ -64,7 +52,6 @@ include('head.html');
                     $data = mysqli_fetch_assoc($query);
                     $count = mysqli_num_rows($query);
                     $no = 1;
-                    echo $sql;
                   ?>
                   <!-- QUERY -->
                   <div class="x_content">
